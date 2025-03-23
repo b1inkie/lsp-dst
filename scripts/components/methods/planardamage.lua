72e7585e66e27351b3d20c030255ddfd08f3a57a
hang wu <wh17720305232@whut.edu.cn>
2025-03-23 13:20:16 +0000

    update scripts/components/methods/planardamage.lua.
    Signed-off-by: hang wu <wh17720305232@whut.edu.cn>

--- scripts/components/methods/planardamage.lua
+++ scripts/components/methods/planardamage.lua
@@ -3,64 +3,64 @@
 ---@class component_planardamage
 local planardamage = {}
 
----
----author: 
+---最终位面伤害：基础位面伤害*乘法修饰总和+加法修饰总和
+---author: mango
 function planardamage:GetDamage()
 end
 
----
----author: 
+---获取调试信息:最终伤害+基础伤害+乘法修饰总和+加法修饰总和
+---author: mango
 function planardamage:GetDebugString()
 end
 
----
+---添加伤害倍率（乘法）
 ---@param src idk # 
 ---@param mult idk # 
 ---@param key idk # 
----author: 
+---author: mango
 function planardamage:AddMultiplier(src,mult,key)
 end
 
----
----author: 
+---获取基础位面伤害
+---author:mango 
 function planardamage:GetBaseDamage()
 end
 
----
----author: 
+---获取当前所有加法加成总和
+---author: mango
 function planardamage:GetBonus()
 end
 
----
+---移除已添加伤害加成（加法部分）
 ---@param src idk # 
 ---@param key idk # 
----author: 
+---author: mango
 function planardamage:RemoveBonus(src,key)
 end
 
----
+---设置基础位面伤害
 ---@param damage idk # 
----author: 
+---author: mango
 function planardamage:SetBaseDamage(damage)
 end
 
----
+---加法修饰，添加固定伤害加成
 ---@param src idk # 
 ---@param bonus idk # 
 ---@param key idk # 
----author: 
+---author: mango
 function planardamage:AddBonus(src,bonus,key)
 end
 
----
----author: 
+---获取当前所有乘法加成
+---author: mango
 function planardamage:GetMultiplier()
 end
 
----
+---移除已添加伤害加成（乘法部分）
 ---@param src idk # 
 ---@param key idk # 
----author: 
+---author: mango
 function planardamage:RemoveMultiplier(src,key)
 end
 
