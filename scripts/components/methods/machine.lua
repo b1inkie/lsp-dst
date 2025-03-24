41f7722c2348fa2fe72319e9c76cedcad3b48014
hang wu <wh17720305232@whut.edu.cn>
2025-03-24 22:01:46 +0800

    machine

--- scripts/components/methods/machine.lua
+++ scripts/components/methods/machine.lua
@@ -3,50 +3,52 @@
 ---@class component_machine
 local machine = {}
 
----
----@param data idk # 
----author: 
+---加载组件状态
+---@param data table # 
+---author: mango
 function machine:OnLoad(data)
 end
 
----
----author: 
+---检查机器是否处于开启状态
+---@return boolean
+---author: mango
 function machine:IsOn()
 end
 
----
----author: 
+---打开机器，启动冷却时间,调用回调函数并触发事件
+---author: mango
 function machine:TurnOn()
 end
 
----
----author: 
+---关闭机器，启动冷却时间，调用回调函数并触发事件
+---author: mango
 function machine:TurnOff()
 end
 
----
----author: 
+---获取调试信息字符串：冷却时间+是否处于冷却状态
+---author: mango
 function machine:GetDebugString()
 end
 
----
----author: 
+---保存组件状态
+---author: mango
 function machine:OnSave()
 end
 
----
----author: 
+---检查机器是否可以交互
+---@return boolean
+---author: mango
 function machine:CanInteract()
 end
 
----
----author: 
+---从实体移除组件时清理相关标签
+---author: mango
 function machine:OnRemoveFromEntity()
 end
 
----
----@param groundonly idk # 
----author: 
+---设置机器是否只能在地面使用
+---@param groundonly boolean # 
+---author: mango
 function machine:SetGroundOnlyMachine(groundonly)
 end
 
