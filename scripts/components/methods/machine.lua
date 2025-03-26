9b3d1301deb63d1b9f99cbfe854efa8a0f937f12
观测分析思考 <10305037+zlzllbz@user.noreply.gitee.com>
2025-03-26 17:05:51 +0800

    关于machine组件的字段和方法的注释

--- scripts/components/methods/machine.lua
+++ scripts/components/methods/machine.lua
@@ -3,50 +3,53 @@
 ---@class component_machine
 local machine = {}
 
----
----@param data idk # 
----author: 
+---组件如何加载数据的方法
+---@param data any #你要保存的数据 
+---author: 观测分析思考
 function machine:OnLoad(data)
 end
 
----
----author: 
+---检查机器是否开启的方法
+---@return boolean
+---author: 观测分析思考
 function machine:IsOn()
 end
 
----
----author: 
+---开启机器的方法
+---author: 观测分析思考
 function machine:TurnOn()
 end
 
----
----author: 
+---关闭机器的方法
+---author: 观测分析思考
 function machine:TurnOff()
 end
 
----
----author: 
+---获取调试信息的方法
+---@return string #具体返回信息："on=%s, cooldowntime=%2.2f, oncooldown=%s"
+---author: 观测分析思考
 function machine:GetDebugString()
 end
 
----
----author: 
+---保存数据的方法
+---author: 观测分析思考
 function machine:OnSave()
 end
 
----
----author: 
+---检查是否可以交互的方法
+---@return boolean
+---author: 观测分析思考
 function machine:CanInteract()
 end
 
----
----author: 
+---当组件从实体移除时调用的方法
+---author: 观测分析思考
 function machine:OnRemoveFromEntity()
 end
 
----
----@param groundonly idk # 
----author: 
+---设置地面使用限制
+---@param groundonly boolean # ture为限制在地面，false为不限制(应该?)
+---author: 观测分析思考
 function machine:SetGroundOnlyMachine(groundonly)
 end
 
