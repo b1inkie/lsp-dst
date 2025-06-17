4763fb5d59e50a011d84540c989bd071fec781b0
b1inkie <zinsserlan@126.com>
2025-06-17 18:34:22 +0800

    update

--- scripts/components/attri/planardefense.lua
+++ scripts/components/attri/planardefense.lua
@@ -1,7 +1,7 @@
 ---@meta
 
 ---@class component_planardefense
----@field inst any #组件挂载的实体
+---@field inst ent #组件挂载的实体
 ---@field basedefense number #基础防御值
 ---@field externalmultipliers table #外部乘法加成列表（SourceModifierList）
 ---@field externalbonuses table #外部加法加成列表（SourceModifierList，additive模式）
