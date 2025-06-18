159c901ff56d48dc9f811cc6cf927b415117a42b
星璃 <15756919+nagisa0704@user.noreply.gitee.com>
2025-06-18 05:10:43 +0000

    !30 update scripts/components/attri/deployable.lua. * update scripts/components/attri/deployable.lua.

--- scripts/components/attri/deployable.lua
+++ scripts/components/attri/deployable.lua
@@ -1,10 +1,10 @@
 ---@meta
 
 ---@class component_deployable
----@field inst idk
----@field mode idk
----@field spacing idk
----@field restrictedtag idk
----@field usegridplacer idk
----@field ondeploy idk
----@field deploytoss_symbol_override idk
+---@field inst ent #组件挂载的实体
+---@field mode number #部署模式（DEPLOYMODE里的一个常量）
+---@field spacing number #部署间距（DEPLOYSPACING里的一个常量）
+---@field restrictedtag tagID|nil #限制部署的标签
+---@field usegridplacer boolean|nil #是否使用网格放置器
+---@field ondeploy fun(inst:ent, pt:Vector3, deployer:ent, rot:number)|nil #部署回调
+---@field deploytoss_symbol_override any #部署动画覆盖
