fc64664844109ef4910a2b44f96e5806c628d195
b1inkie <zinsserlan@126.com>
2025-08-12 16:12:09 +0800

    update

--- scripts/types.lua
+++ scripts/types.lua
@@ -3,6 +3,9 @@
 ---@class Vector3
 ---@overload fun(x: number, y: number, z: number):Vector3
 ---@operator add(Vector3): Vector3
+---@operator sub(Vector3): Vector3
+---@operator mul(number): number
+---@operator div(number): number
 ---@field x number
 ---@field y number
 ---@field z number
