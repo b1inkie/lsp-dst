848a348e58202a6f676410f962a6525a5199db06
LunarSolar <kageno13927@yeah.net>
2025-10-13 15:39:01 +0800

    update Light

--- scripts/sys_components/Light.lua
+++ scripts/sys_components/Light.lua
@@ -4,26 +4,22 @@
 local Light = {}
 
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:SetFalloff(...) end
+---设置光照衰减
+---@param falloff number # (0, inf) float
+---author: Runar
+function Light:SetFalloff(falloff) end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetRadius(...) end
+---获取光照范围
+---@return number
+---author: Runar
+function Light:GetRadius() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:SetColour(...) end
+---设置光照颜色
+---@param r number
+---@param g number
+---@param b number
+---author: Runar
+function Light:SetColour(r, g, b) end
 
 ---
 ---启用光照,注意如果仅手持发光,则需在 `inventoryitem:SetOnDroppedFn` 中,也调用此方法
@@ -32,73 +28,54 @@ function Light:SetColour(...) end
 ---author: lan
 function Light:Enable(enable) end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetFalloff(...) end
+---获取光照衰减
+---@return number # (0, inf) float
+---author: Runar
+function Light:GetFalloff() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetColour(...) end
+---获取光照颜色
+---@return number
+---@return number
+---@return number
+---author: Runar
+function Light:GetColour() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:SetIntensity(...) end
+---设置光照强度
+---@param intensity number # (0, 1] float
+---author: Runar
+function Light:SetIntensity(intensity) end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:SetRadius(...) end
+---设置光照范围
+---@param radius number
+---author: Runar
+function Light:SetRadius(radius) end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:SetDisableOnSceneRemoval(...) end
+---实体从场景移除后禁用光照
+---@param disable boolean
+---author: Runar
+function Light:SetDisableOnSceneRemoval(disable) end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:IsEnabled(...) end
+---光照是否启用
+---@return boolean
+---author: Runar
+function Light:IsEnabled() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetIntensity(...) end
+---获取光照强度
+---@return number # (0, 1] float
+---author: Runar
+function Light:GetIntensity() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetCalculatedRadius(...) end
+---获取计算后的光照范围
+---author: Runar
+function Light:GetCalculatedRadius() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:GetDisableOnSceneRemoval(...) end
+---实体从场景移除后是否禁用光照
+---@return boolean
+---author: Runar
+function Light:GetDisableOnSceneRemoval() end
 
----
----UNKNOWN
----
----@param ... any
----author:
-function Light:EnableClientModulation(...) end
+---启用客户端调制
+---@param enable boolean
+---author: Runar
+function Light:EnableClientModulation(enable) end
 
