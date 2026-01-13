6628e45a38b3fc4d3bc8905787b715fce5797fbc
LunarSolar <kageno13927@yeah.net>
2026-01-13 19:13:26 +0800

    update

--- scripts/sys_components/Follower.lua
+++ scripts/sys_components/Follower.lua
@@ -4,11 +4,18 @@
 local Follower = {}
 
 ---
----UNKNOWN
+---跟随到通道
 ---
----@param ... any
----author:
-function Follower:FollowSymbol(...) end
+---@param GUID integer
+---@param symbol string|hashed_code|nil
+---@param offset_x number|nil
+---@param offset_y number|nil
+---@param offset_z number|nil
+---@param followlayered boolean|nil # 跟随到通道所在层级
+---@param UNKNOWN boolean|nil
+---@param followframe integer|nil
+---author: Runar
+function Follower:FollowSymbol(GUID, symbol, offset_x, offset_y, offset_z, followlayered, UNKNOWN, followframe) end
 ---
 ---UNKNOWN
 ---
