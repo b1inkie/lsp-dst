9f78e10b1a69760cc21bffa89029ad739883d607
b1inkie <zinsserlan@126.com>
2026-02-15 17:05:55 +0800

    update

---
+++ scripts/global/TheShard.lua
@@ -0,0 +1,51 @@
+---@meta
+
+---@class TheShard
+TheShard = {}
+
+ShardProxy = TheShard
+
+---
+---@param ... idk # 
+---author:
+function TheShard:IsPlayer(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:IsMigrating(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:IsSecondary(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:SetSecondaryLoading(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:IsMaster(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:GetDefaultShardEnabled(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:StartMigration(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:GetSecondaryShardPlayerCounts(...) end
+
+---
+---@param ... idk # 
+---author:
+function TheShard:GetShardId(...) end
